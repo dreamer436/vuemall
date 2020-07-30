84e60bc5993b98081078f4bc312c6d31c3e86e6a
Your Name <you@example.com>
2020-07-30 23:40:27 +0800

    完成了开发

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export function request(config) {
     // 1.创建axios实例
     const instance = axios.create({
-        baseURL:'http://152.136.185.210:8000/api/n3',
+        baseURL:'http://139.261.366.189:8000/api/n3',
         timeout:5000
     })
     // axios拦截器
